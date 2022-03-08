3cefae5a5d06f26bbb09b836f0b69bda7386dddf
Sunday P. Afolabi <spog4true@gmail.com>
2022-03-08 11:12:11 +0100

    Add count.html, and add event to the button click

--- logic.js
+++ logic.js
@@ -1,5 +1,13 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    let counter = localStorage.getItem("counter");
+    const display = document.getElementById("template");
+    display.innerHTML = counter;
+    document.querySelector(".button").addEventListener("click", () => {
+        counter++;
+        display.innerHTML = counter;
+        localStorage.setItem("counter", counter);
+    });
 });
+
